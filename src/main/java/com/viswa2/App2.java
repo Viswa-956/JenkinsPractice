70ad02d8ba996660eed3e0ce8cf05fdc96bde615
Kumar M Prasanna <prasanna.kumar@nagra.com>
2023-06-13 10:46:32 +0530

    test commit

---
+++ src/main/java/com/viswa2/App2.java
@@ -0,0 +1,13 @@
+package com.viswa2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
